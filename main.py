f4a2306195c0e1dfdbaaaab1b0ddad63c27d5a66
Destin Moulton <destin.moulton@gmail.com>
2018-02-23 15:11:57 -0700

    Added cutoff time check.

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import pyowm
 import requests
-from datetime import datetime
+from datetime import datetime, timedelta
 from dateutil import tz
 from pprint import pprint
 
@@ -18,9 +18,21 @@ res = requests.get(api_url, params=params)
 print(res.url)
 
 from_zone = tz.gettz('UTC')
-to_zone = tz.gettz('America/New_York')
+this_zone = tz.gettz('America/Denver')
+
+now = datetime.now()
+current_day = now.date()
+tomorrow = timedelta(days=1) + now
+tomorrow_cutoff_time = datetime(
+    tomorrow.year, tomorrow.month, tomorrow.day, 12, 0, 0, 0).replace(tzinfo=this_zone)
+
+
 if res.status_code == 200:
     data = res.json()
 
     for fcast in data["list"]:
-        pprint(fcast)
+        dt = datetime.fromtimestamp(fcast['dt'], from_zone)
+        forecast_time = dt.astimezone(this_zone)
+
+        if forecast_time < tomorrow_cutoff_time:
+            pprint(fcast["main"]["temp_min"])
