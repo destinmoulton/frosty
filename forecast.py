4c0412901192a4a5f6d36a022dd8d2b26a7f0dd7
Destin Moulton <destin.moulton@gmail.com>
2018-02-26 14:09:04 -0700

    Moved timezone calculations into config.

--- forecast.py
+++ forecast.py
@@ -1,20 +1,17 @@
+import config
+
 
 def get_minimum_temperature(data):
-    from_zone = tz.gettz('UTC')
-    this_zone = tz.gettz('America/Denver')
 
     now = datetime.now()
     current_day = now.date()
     tomorrow = timedelta(days=1) + now
     tomorrow_cutoff_time = datetime(
-        tomorrow.year, tomorrow.month, tomorrow.day, 12, 0, 0, 0).replace(tzinfo=this_zone)
+        tomorrow.year, tomorrow.month, tomorrow.day, 12, 0, 0, 0).replace(tzinfo=config.zones["local"])
 
-    if res.status_code == 200:
-        data = res.json()
+    for fcast in data["list"]:
+        dt = datetime.fromtimestamp(fcast['dt'], config.zones["orig"])
+        forecast_time = dt.astimezone(config.zones["local"])
 
-        for fcast in data["list"]:
-            dt = datetime.fromtimestamp(fcast['dt'], from_zone)
-            forecast_time = dt.astimezone(this_zone)
-
-            if forecast_time < tomorrow_cutoff_time:
-                pprint(fcast["main"]["temp_min"])
+        if forecast_time < tomorrow_cutoff_time:
+            pprint(fcast["main"]["temp_min"])
