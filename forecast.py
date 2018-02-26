6d4fdc04b315369b4b65ab3013a835a03a38d624
Destin Moulton <destin.moulton@gmail.com>
2018-02-26 16:06:46 -0700

    Moved the time calculation to the helper.

--- forecast.py
+++ forecast.py
@@ -4,21 +4,14 @@ from pprint import pprint
 import config
 
 
-def get_minimum_temperature(data):
-
-    now = datetime.now()
-    current_day = now.date()
-    tomorrow = timedelta(days=1) + now
-    tomorrow_cutoff_time = datetime(
-        tomorrow.year, tomorrow.month, tomorrow.day, 12, 0, 0, 0).replace(tzinfo=config.zones["local"])
-
+def get_minimum_temperature(data, start_datetime, cutoff_datetime):
     min_temp = 100
     min_time = {}
     for fcast in data["list"]:
         dt = datetime.fromtimestamp(fcast['dt'], config.zones["orig"])
         forecast_time = dt.astimezone(config.zones["local"])
 
-        if forecast_time < tomorrow_cutoff_time:
+        if start_datetime < forecast_time < cutoff_datetime:
             if fcast["main"]["temp_min"] < min_temp:
                 min_temp = fcast["main"]["temp_min"]
                 min_time = forecast_time
