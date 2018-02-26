62ec6a5c4e6efca5db2ee00a33ac584b2beaba72
Destin Moulton <destin.moulton@gmail.com>
2018-02-26 16:05:42 -0700

    Added start_datetime

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
 
 
 from api import get_weather_data
+from datetimehelper import get_cutoff_datetime, get_start_datetime
 from emailer import send_email
+
 from forecast import get_minimum_temperature
+from plotter import plot_temperature_graph
+
+
+start_datetime = get_start_datetime()
+cutoff_datetime = get_cutoff_datetime()
 
 data = get_weather_data()
-min_data = get_minimum_temperature(data)
+plot_temperature_graph(data, start_datetime, cutoff_datetime)
+min_data = get_minimum_temperature(data, start_datetime, cutoff_datetime)
 send_email(min_data)
