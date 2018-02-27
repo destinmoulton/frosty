4cf4aaf05279b66022eadc42dbaa7be3aa6e17a3
Destin Moulton <destin.moulton@gmail.com>
2018-02-27 10:53:22 -0700

    Altered names to reflect refactoring.

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
 
 
 from api import get_weather_data
-from datetimehelper import get_cutoff_datetime, get_start_datetime
+from datetimehelper import get_end_datetime, get_start_datetime
 from emailer import send_email
 
-from forecast import get_minimum_temperature
-from plotter import plot_temperature_graph
+from forecast import get_min_temp_and_time
+from plotter import plot_forecast_chart
 
 
 start_datetime = get_start_datetime()
-cutoff_datetime = get_cutoff_datetime()
+end_datetime = get_end_datetime()
 
 data = get_weather_data()
-plot_temperature_graph(data, start_datetime, cutoff_datetime)
-min_data = get_minimum_temperature(data, start_datetime, cutoff_datetime)
+plot_forecast_chart(data["list"], start_datetime, end_datetime)
+min_data = get_min_temp_and_time(data["list"], start_datetime, end_datetime)
 send_email(min_data)
