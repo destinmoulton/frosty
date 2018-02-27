6265629e8b0c150c1ab3709ea53d97bd917bc992
Destin Moulton <destin.moulton@gmail.com>
2018-02-27 10:33:12 -0700

    Renamed vars and added docstring describing the method.

--- forecast.py
+++ forecast.py
@@ -4,14 +4,31 @@ from pprint import pprint
 import config
 
 
-def get_minimum_temperature(data, start_datetime, cutoff_datetime):
+def get_minimum_temperature(forecast_data, start_datetime, end_datetime):
+    '''Get the minimum temperature and time within a datetime range.
+
+    Parameters
+    ----------
+    forecast_data : list
+        List of dicts containing the future weather data.
+    start_datetime : datetime
+        The initial time to start testing temps
+    end_datetime : datetime
+        The time to stop testing temps
+
+    Returns
+    -------
+    tuple
+        Minimum temp, Time min temp occurred
+
+    '''
     min_temp = 100
     min_time = {}
-    for fcast in data["list"]:
+    for fcast in forecast_data:
         dt = datetime.fromtimestamp(fcast['dt'], config.zones["orig"])
         forecast_time = dt.astimezone(config.zones["local"])
 
-        if start_datetime < forecast_time < cutoff_datetime:
+        if start_datetime < forecast_time < end_datetime:
             if fcast["main"]["temp_min"] < min_temp:
                 min_temp = fcast["main"]["temp_min"]
                 min_time = forecast_time
