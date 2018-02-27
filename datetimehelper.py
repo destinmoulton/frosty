9c80ae16f4c4125171dc8991b1612c4d22dce477
Destin Moulton <destin.moulton@gmail.com>
2018-02-27 10:47:55 -0700

    Added parameters to docstring and changed param names.

--- datetimehelper.py
+++ datetimehelper.py
@@ -4,12 +4,24 @@ import config
 
 
 def get_start_datetime():
+    '''Get the datetime to start forecast analysis
+
+    Returns
+    -------
+    datetime
+    '''
     now = datetime.now()
     return datetime(
         now.year, now.month, now.day, 12, 0, 0, 0).replace(tzinfo=config.zones["local"])
 
 
-def get_cutoff_datetime():
+def get_end_datetime():
+    '''Get the end datetime for forecast analysis
+
+    Returns
+    -------
+    datetime
+    '''
     now = datetime.now()
     tomorrow = timedelta(days=1) + now
     return datetime(
