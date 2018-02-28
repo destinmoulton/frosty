30c039b7045c4b7a8bc2068fd2a913ec40cc8818
Destin Moulton <destin.moulton@gmail.com>
2018-02-28 10:46:10 -0700

    Added message if the data acquisition fails.

--- frosty.py
+++ frosty.py
@@ -12,6 +12,11 @@ start_datetime = get_start_datetime()
 end_datetime = get_end_datetime()
 
 data = get_weather_data()
-plot_forecast_chart(data["list"], start_datetime, end_datetime)
-min_data = get_min_temp_and_time(data["list"], start_datetime, end_datetime)
-send_email(min_data)
+
+if "list" in data:
+    plot_forecast_chart(data["list"], start_datetime, end_datetime)
+    min_data = get_min_temp_and_time(
+        data["list"], start_datetime, end_datetime)
+    send_email(min_data)
+else:
+    print("FROSTY ERROR: Failed to acquire weather data.")
